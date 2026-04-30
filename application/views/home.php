0cef1f93d2a06f51546e2bed3ad06dca7b5c635d
Tarmidzi Bariq <81232207+tarmidzibariq@users.noreply.github.com>
2026-04-30 14:56:31 +0700

    first commit

---
+++ application/views/home.php
@@ -0,0 +1,31 @@
+<?php
+echo $this->session->flashdata('error');
+?>
+
+<ul class="collection">
+    <?php foreach ($home_post as $data):?>
+        <li class="collection-item avatar">
+            <img src="<?= site_url("/upload/post/".$data['filename']);?>" class="circle">
+            <p class="title"><?= $data['name']; ?></p>
+            <small><?= $data['description']?></small>
+            <a href="<?= site_url("welcome/index/".$data['id']); ?>" class="secondary_content">
+                <i class="material-icons" style="margin-left: 50px">visibility</i>
+            </a>
+        </li>
+    <?php endforeach; ?>
+
+<div class="col s6" style="margin-top: 10px; margin-bottom: 10px;">
+    <a href="<?= site_url('welcome/deleteAll/'); ?>" class="btn red" id="deleteLink">Delete All</a>
+</div>
+
+<script>
+document.getElementById("deleteLink").addEventListener("click", function(event) {
+    var confirmation = confirm('Apakah ingin dihapus semua?');
+    if (!confirmation) {
+        event.preventDefault();
+    }
+});
+</script>
+
+
+</ul>
